73aa84bf244d18e683d0de2ee1936716ea6653d2
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-06-29 21:07:54 +0700

    :memo: Implemented user signup API setup.

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,15 +1,30 @@
 import { Link as MUILink } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useCreateUser } from '../../hooks/useCreateUser';
 import Auth from './Auth';
 
-function Signup() {
+const Signup = () => {
+  const [createUser] = useCreateUser();
+
   return (
-    <Auth submitLabel="Signup" onSubmit={async () => {}}>
+    <Auth
+      submitLabel="Signup"
+      onSubmit={async ({ email, password }) => {
+        await createUser({
+          variables: {
+            createUserInput: {
+              email,
+              password,
+            },
+          },
+        });
+      }}
+    >
       <Link to={'/login'} style={{ alignSelf: 'center' }}>
         <MUILink>Login</MUILink>
       </Link>
     </Auth>
   );
-}
+};
 
 export default Signup;
